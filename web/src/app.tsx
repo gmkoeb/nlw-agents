86fb28fb2a02d8d31ac8d53ba948c4db72b56d82
Gabriel Manika <gmkoeb@gmail.com>
2025-07-08 14:30:15 -0300

    feat: install and configure shadcnui

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <Button variant={'secondary'}>Hello World</Button>
+    </div>
+  )
 }
