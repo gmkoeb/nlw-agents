9df37736f554635a734b1652b730046dff2288f0
Gabriel Manika <gmkoeb@gmail.com>
2025-07-08 11:40:19 -0300

    feat: create rooms list route

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,12 +6,15 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(getRoomsRoute)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
