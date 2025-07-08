15c93b7a95f7e3078f628ecf5b9ea68c7a1b4745
Gabriel Manika <gmkoeb@gmail.com>
2025-07-08 11:16:55 -0300

    feat: configure postgres database with docker and drizzle-orm
    Created database connection script + database url env
    
    Created seed script to populate the development database with mock
    data
    
    Configured docker-compose with the pgvector image. This image comes
    with the vector extension for postgresql to use vector similarity search
    in queries

---
+++ server/src/db/seed.ts
@@ -0,0 +1,22 @@
+/** biome-ignore-all lint/suspicious/noConsole: only used in dev */
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+await reset(db, schema)
+
+await seed(db, schema).refine((f) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.companyName(),
+        description: f.jobTitle(),
+      },
+    },
+  }
+})
+
+await sql.end()
+
+console.log('Database Seeded')
