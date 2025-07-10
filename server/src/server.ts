5f268b7c5ea230fa923eec14d59057fc195e7435
Gabriel Manika <gmkoeb@gmail.com>
2025-07-10 15:14:11 -0300

    feat: handle audio upload and integration with google gemini api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -10,17 +11,19 @@ import { createRoomRoute } from './http/routes/create-room.ts'
 import { createRoomQuestionRoute } from './http/routes/create-room-question.ts'
 import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
-
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+app.register(fastifyMultipart)
 
 app.register(getRoomsRoute)
 app.register(getRoomQuestionsRoute)
 app.register(createRoomRoute)
 app.register(createRoomQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
